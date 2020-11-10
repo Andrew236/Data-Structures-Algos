3634fb75b02a12c23da668a32c36ee1a3f8befdf
Andrew <whitandr@oregonstate.edu>
2020-11-09 19:06:31 -0800

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,22 @@
+
+
+def quick_sort(my_list):
+    length = len(my_list)
+    if length <=1:
+        return my_list
+    else:
+        pivot = my_list.pop()
+
+    greater = []
+    less = []
+
+    for item in my_list:
+        if item > pivot:
+            greater.append(item)
+        else:
+            less.append(item)
+
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print(quick_sort([1,3,4,5,6,2,19,15,20]))
